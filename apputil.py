1b3bf85a21526fecb2b3ac2e6d9c3037ed11fb08
atdiallo7 <atdiallo@iu.edu>
2026-09-16 17:39:54 -0400

    Function to calculate the number of ways to make change for a given amount of cents using specified coin types.
    Function to find the name corresponding to the
    lowest score.
    
    Function to sort names based on their scores in
    descending order.

--- apputil.py
+++ apputil.py
@@ -3,11 +3,16 @@ import numpy as np
 
 # update/add code below ...
 
-def ways(n):
-    return None
+def ways(cents, coin_types=[1, 5]):
+    counts = [1] + [0] * cents
+    for coin in coin_types:
+        for amount in range(coin, cents + 1):
+            counts[amount] += counts[amount - coin]
+    return counts[cents]
 
 def lowest_score(names, scores):
-    return None
+    return names[np.argmin(scores)]
 
 def sort_names(names, scores):
-    return None
+    order = np.argsort(scores)[::-1]
+    return list(names[order])
